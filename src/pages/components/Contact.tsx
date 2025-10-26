a35ba9590d84dab95fab25c92d0e240a6cf515e3
Shiva Sajay <shivasajay007@gmail.com>
2025-10-26 22:08:31 +0530

    clean the contact us section

--- src/pages/components/Contact.tsx
+++ src/pages/components/Contact.tsx
@@ -7,10 +7,10 @@ const Contact = () => {
         CONTACT US
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 text-lg">
-        <div>
+        {/* <div>
           <h3 className="font-semibold mb-2">Email</h3>
           <p className="text-muted-foreground">iedc@cusat.ac.in</p>
-        </div>
+        </div> */}
         <div>
           <h3 className="font-semibold mb-2">Address</h3>
           <p className="text-muted-foreground">
@@ -20,7 +20,7 @@ const Contact = () => {
           </p>
         </div>
         <div className="md:text-right">
-          <h3 className="font-semibold mb-2">Follow Us</h3>
+          {/* <h3 className="font-semibold mb-2">Follow Us</h3>
           <div className="flex gap-4 md:justify-end">
             <a href="#" className="text-muted-foreground hover:text-foreground">
               <Instagram className="h-8 w-8" />
@@ -28,7 +28,7 @@ const Contact = () => {
             <a href="#" className="text-muted-foreground hover:text-foreground">
               <Linkedin className="h-8 w-8" />
             </a>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
